31d52a68b877542e906daabd50e4ddd2a6e4b717
numesh <numesh28@gmail.com>
2025-03-20 23:24:25 +0530

    created my branch

---
+++ numesh/erics_python/chap2.2.py
@@ -0,0 +1,8 @@
+#2-2. Simple Messages: Store a message in a variable, and print that message.
+     #Then change the value of your variable to a new message, and print the new message
+
+country="india is my country"
+print (country)
+
+country="bharat is my country"
+print (country)
