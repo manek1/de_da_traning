59fedfbf9d0d4f9948b7a6af861f2eb9cb7f67ad
madhuri <madhuripatil1025@gmail.com>
2025-03-25 19:56:45 +0530

    done

---
+++ madhuri/Erics_python/chp3.1.py
@@ -0,0 +1,17 @@
+#3-1. Names: Store the names of a few of your friends in a list called names. Print
+#each person’s name by accessing each element in the list, one at a time.
+
+
+name=['Megha','Avinash','Amar','Urmila']
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+
+
+#using for loop
+
+name=['Megha','Avinash','Amar','Urmila']
+for names in name:
+    print(names)
+
