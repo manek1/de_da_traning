076c9ee372cf845e32ee8e004f8f156a446ce53d
AshwathiN <ashwathinair28062@gmail.com>
2025-04-22 13:08:08 +0400

    Mni project on Python-LMS begun.

---
+++ Ashwathi/Library_Management_System/library.py
@@ -0,0 +1,19 @@
+'''Module 2: library.py
+
+This will have the Library class with:
+	•	_init_: to create a collection of books
+	•	add_book(book): to add a book to the library
+	•	borrow_book(title): to borrow a book
+	•	return_book(title): to return a book
+	•	display_books(): to display all books with availability
+
+Also write a test script (main.py) to:
+	•	Create book objects
+	•	Add them to the library
+	•	Borrow and return a few books
+	•	Display current status of library
+'''
+
+class Library():
+    #
+    def __init__(self,....):
