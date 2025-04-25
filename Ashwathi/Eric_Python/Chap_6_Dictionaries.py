6cabe19b14df0e60b407a53eda9d0400ffcfe77f
AshwathiN <ashwathinair28062@gmail.com>
2025-04-25 08:46:27 +0400

    Erics Python Chap 6 on Dictionaries

---
+++ Ashwathi/Eric_Python/Chap_6_Dictionaries.py
@@ -0,0 +1,74 @@
+'''6-1. Person: Use a dictionary to store information about a person you know.
+Store their first name, last name, age, and the city in which they live. You
+should have keys such as first_name, last_name, age, and city. Print each
+piece of information stored in your dictionary.'''
+
+person={'first_name':'Ashwathi','last_name':'Nair','age':'27','city':'Dubai'}
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
+
+'''6-2. Favorite Numbers: Use a dictionary to store people’s favorite numbers.
+Think of five names, and use them as keys in your dictionary. Think of a favorite 
+number for each person, and store each as a value in your dictionary. Print 
+each person’s name and their favorite number. For even more fun, poll a few 
+friends and get some actual data for your program.'''
+
+fav_no={'Ash':'9','Pra':'369','Bob':'7','Kian':'78'}
+print(fav_no)
+
+'''6-3. Glossary: A Python dictionary can be used to model an actual dictionary.
+However, to avoid confusion, let’s call it a glossary.
+•	 Think of five programming words you’ve learned about in the previous 
+chapters. Use these words as the keys in your glossary, and store their 
+meanings as values.
+•	 Print each word and its meaning as neatly formatted output. You might 
+print the word followed by a colon and then its meaning, or print the word 
+on one line and then print its meaning indented on a second line. Use the 
+newline character (\n) to insert a blank line between each word-meaning 
+pair in your output.'''
+
+glossary={
+    'lists':"Collection of iems accessed from position",
+    'dictionary':"Collection of items in key-value form",
+    'classes':"Its a blueprint to create objects.  "
+}
+print("The glossary of items are: ")
+for key in glossary:
+    print(key+":\n\t"+glossary[key])
+
+'''6-4. Glossary 2: Now that you know how to loop through a dictionary, clean 
+up the code from Exercise 6-3 (page 102) by replacing your series of print
+statements with a loop that runs through the dictionary’s keys and values.
+When you’re sure that your loop works, add five more Python terms to your 
+glossary. When you run your program again, these new words and meanings 
+should automatically be included in the output.'''
+
+'''6-5. Rivers: Make a dictionary containing three major rivers and the country 
+each river runs through. One key-value pair might be 'nile': 'egypt'.
+•	 Use a loop to print a sentence about each river, such as The Nile runs 
+through Egypt.
+•	 Use a loop to print the name of each river included in the dictionary.
+•	 Use a loop to print the name of each country included in the dictionary.'''
+
+rivers={'Nile':'Egypt','Ganga':'Northern India','Kaveri':'South India'}
+for key,value in rivers.items():
+    print("\nThe river "+key+" runs through "+value)
+print("The list of rivers are: ")
+for key in rivers:
+    print('\t'+key)
+print("The list of countries are: ")
+for value in rivers.values():
+    print('\t'+value)
+
+'''6-6. Polling: Use the code in favorite_languages.py (page 104).
+•	 Make a list of people who should take the favorite languages poll. Include 
+some names that are already in the dictionary and some that are not.
+•	 Loop through the list of people who should take the poll. If they have 
+already taken the poll, print a message thanking them for responding.
+If they have not yet taken the poll, print a message inviting them to take 
+the poll.'''
+
+
+
