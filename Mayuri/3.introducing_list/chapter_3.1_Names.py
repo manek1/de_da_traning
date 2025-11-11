936828bc006b5a7f50cab512dff85bbbdc0b8b70
manek1 <mayuriemp1996@gmail.com>
2025-11-12 02:15:13 +0530

    added 3 chapter 'list' exercises in Mayuri folder

---
+++ Mayuri/3.introducing_list/chapter_3.1_Names.py
@@ -0,0 +1,8 @@
+# friends_names_list
+names = ["Kiran", "Ashwathi", "Pranita", "Pooja"]
+
+# Print each name one by one
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
---
+++ Mayuri/3.introducing_list/chapter_3.3_Your_Own_List.py
@@ -0,0 +1,13 @@
+
+
+# List of favorite modes of transportation
+vehicles = ["THAR car", "Royal enfield bike", "Activa scooty"]
+
+# Print all messages together
+message1 = f"I would like to own a {vehicles[0]}."
+message2 = f"I would like to own a {vehicles[1]}."
+message3 = f"I would like to own a {vehicles[2]}."
+
+print(message1)
+print(message2)
+print(message3)
